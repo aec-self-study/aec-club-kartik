9812d7b683f7149b1762329ad1fa238cfd0c5411
kartikS1467 <155270867+kartikS1467@users.noreply.github.com>
2024-04-21 20:35:43 +1000

    Update table alias

--- customers.sql
+++ customers.sql
@@ -1,10 +1,10 @@
 SELECT 
-  c.id, 
-  c.name, 
-  c.email, 
+  co.id, 
+  co.name, 
+  co.email, 
   COUNT(o.id), 
   MIN(o.created_at)
-FROM `analytics-engineers-club.coffee_shop.customers` c
+FROM `analytics-engineers-club.coffee_shop.customers` co
   JOIN analytics-engineers-club.coffee_shop.orders o ON c.id = o.customer_id
 WHERE c.name IN ('Catherine Huff', 'Aaron Newton', 'Jacob Smith', 'Shelly Black', 'Alex Lopez')
 GROUP BY ALL
