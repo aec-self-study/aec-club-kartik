1a21d2e031d847781ecda4eada832082c2fdaf6f
kartikS1467 <155270867+kartikS1467@users.noreply.github.com>
2024-04-28 05:14:39 +0000

    Initialising dbt project and creating sample file

---
+++ v1_dbt_project/models/customers.sql
@@ -0,0 +1,22 @@
+WITH
+    orders AS (
+                  SELECT
+                      customer_id,
+                      SUM(total) AS total_sales,
+                      COUNT(id) AS total_orders,
+                      MIN(created_at) AS first_order_date
+                  FROM analytics-engineers-club.coffee_shop.orders
+                  GROUP BY 1
+    )
+
+
+SELECT 
+    c.id,
+    c.name,
+    c.email,
+    o.total_orders,
+    o.total_sales,
+    o.first_order_date
+FROM analytics-engineers-club.coffee_shop.customers c
+    JOIN orders o ON c.id = o.customer_id
+ORDER BY o.first_order_date
